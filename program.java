9f140acea5d0356e8c4353fd760a5673e86929d2
Denis Lodygin <Alirka94@mail.ru>
2024-06-10 00:04:04 +0500

    1231

--- program.java
+++ program.java
@@ -1,8 +1,22 @@
 package java_lekcia_01;
 
-public class program {
+import java.util.Scanner;
 
+public class program {
     public static void main(String[] args) {
-        System.out.println("world");
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("name: ");
+        String name = iScanner.nextLine();
+        System.out.printf("Привет, %s \n", name);
+        iScanner.close();
+        // boolean f  = true & false; 
+        // System.out.println(f);
+
+        // boolean f  = true ^ true; 
+        // System.out.println(f); 
+
+        
+
     }
 }
+
